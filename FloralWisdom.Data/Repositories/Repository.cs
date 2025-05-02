f84048a096def65ac0648904a5272df81b0959a8
AnkaBananka08 <aneliyapapazova08@pmgkk.com>
2025-05-03 00:18:43 +0300

    Added folder extensions with class and repository

---
+++ FloralWisdom.Data/Repositories/Repository.cs
@@ -0,0 +1,138 @@
+﻿#region Usings
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace FloralWisdom.Data.Repositories
+{
+
+	public class ProfRepository<TType, TId>(
+		FloralWisdomDbContext dbContext)
+		: IRepository<TType, TId>
+		where TType : class
+	{
+		private readonly DbSet<TType> dbSet
+			= dbContext.Set<TType>();
+
+		public void Add(TType item)
+		{
+			dbSet.Add(item);
+			dbContext.SaveChanges();
+		}
+
+		public async Task AddAsync(TType item)
+		{
+			await dbSet.AddAsync(item);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public void AddRange(TType[] items)
+		{
+			dbSet.AddRange(items);
+			dbContext.SaveChanges();
+		}
+
+		public async Task AddRangeAsync(TType[] items)
+		{
+			await dbSet.AddRangeAsync(items);
+			await dbContext.SaveChangesAsync();
+		}
+
+		public bool Delete(TType entity)
+		{
+			dbSet.Remove(entity);
+			int changes = dbContext.SaveChanges();
+
+			return changes > 0;
+		}
+
+		public async Task<bool> DeleteAsync(TType entity)
+		{
+			dbSet.Remove(entity);
+			int changes = await dbContext.SaveChangesAsync();
+
+			return changes > 0;
+		}
+
+		public TType? FirstOrDefault(Func<TType, bool> predicate)
+		{
+			TType? entity = dbSet
+				.FirstOrDefault(predicate);
+
+			return entity;
+		}
+
+		public async Task<TType?> FirstOrDefaultAsync(Expression<Func<TType, bool>> predicate)
+		{
+			TType? entity = await dbSet
+				 .FirstOrDefaultAsync(predicate);
+
+			return entity;
+		}
+
+		public IEnumerable<TType> GetAll()
+		{
+			return dbSet.ToArray();
+		}
+
+		public async Task<IEnumerable<TType>> GetAllAsync()
+		{
+			return await dbSet.ToArrayAsync();
+		}
+
+		public IQueryable<TType> GetAllAttached()
+		{
+			return dbSet.AsQueryable();
+		}
+
+		public TType? GetById(TId id)
+		{
+			TType? entity = dbSet
+				.Find(id);
+
+			return entity;
+		}
+
+		public async Task<TType?> GetByIdAsync(TId id)
+		{
+			TType? entity = await dbSet
+				.FindAsync(id);
+
+			return entity;
+		}
+
+		public bool Update(TType item)
+		{
+			try
+			{
+				dbSet.Attach(item);
+				dbContext.Entry(item).State = EntityState.Modified;
+				dbContext.SaveChanges();
+
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> UpdateAsync(TType item)
+		{
+			try
+			{
+				dbSet.Attach(item);
+				dbContext.Entry(item).State = EntityState.Modified;
+				await dbContext.SaveChangesAsync();
+
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ FloralWisdom.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿#region Usings
+
+using FloralWisdom.Data.Repositories;
+using FloralWisdom.Services.Implementations;
+using FloralWisdom.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using FloralWisdom.Models.Entities;
+
+#endregion
+
+namespace FloralWisdom.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+	public static IServiceCollection RegisterRepositories(
+		this IServiceCollection services)
+	{
+		services.AddScoped<IRepository<Plant, int>, ProfRepository<Plant, int>>();
+		services.AddScoped<IRepository<CareReminder, int>, ProfRepository<CareReminder, int>>();
+		services.AddScoped<IRepository<DiseaseReport, int>, ProfRepository<DiseaseReport, int>>();
+		services.AddScoped<IRepository<User, int>, ProfRepository<User, int>>();
+		services.AddScoped<IRepository<UserPlant, object>, ProfRepository<UserPlant, object>>();
+		services.AddScoped<IRepository<UserRequest, int>, ProfRepository<UserRequest, int>>();
+
+		return services;
+	}
+
+	public static IServiceCollection RegisterUserDefinedServices(
+		this IServiceCollection services)
+	{
+		services.AddScoped<IUserService, UserService>();
+		services.AddScoped<IPlantService, PlantService>();
+		services.AddScoped<ICareReminderService, CareReminderService>();
+		services.AddScoped<IDiseaseReportService, DiseaseReportService>();
+		services.AddScoped<IUserRequestService, UserRequestService>();
+
+		return services;
+	}
+}
